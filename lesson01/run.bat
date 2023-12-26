e963e1d4210540c15f0d3ea660d599ab40b330b6
傅炜舜 <145452598+fashionfu@users.noreply.github.com>
2023-12-26 15:50:56 +0800

    Add files via upload

---
+++ lesson01/run.bat
@@ -0,0 +1,2 @@
+start http://localhost:4111/lesson00/index00.html
+node -s localhost:4111
---
+++ lesson07-渐变纹理绘制/canvas.js
@@ -0,0 +1,7 @@
+
+      window.onload = function() {
+        var c = document.getElementById("myCanvas");
+        var ctx = c.getContext("2d");
+        ctx.fillStyle = "#ff0000";
+        ctx.fillRect(0, 0, 1, 100);
+      };
